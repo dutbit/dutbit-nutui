6bd3d37d70fea2f9a529c842252c5962065fdc47
Flinx-LY <2102095923@qq.com>
2021-11-07 13:34:58 +0800

    style: 更改vue文件名为首字母大写

--- src/router.js
+++ src/router.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import home from './components/home.vue'
-import func from './components/func.vue'
-import message from './components/message.vue'
-import my from './components/my.vue'
-import about from './components/about.vue'
-import raiseIssue from './components/raiseIssue.vue'
+import Home from './components/Home.vue'
+import Func from './components/Func.vue'
+import Message from './components/Message.vue'
+import My from './components/My.vue'
+import About from './components/About.vue'
+import SelectIssueType from './components/SelectIssueType.vue'
 
 const routes = [
-  { path: '/', component: home, meta: { pageTitle: '首页' } },
-  { path: '/home', component: home, meta: { pageTitle: '首页' } },
-  { path: '/func', component: func, meta: { pageTitle: '功能' } },
-  { path: '/message', component: message, meta: { pageTitle: '消息' } },
-  { path: '/my', component: my, meta: { pageTitle: '我的' } },
-  { path: '/about', component: about, meta: { pageTitle: '关于' } },
-  { path: '/func/raiseIssue', component: raiseIssue, meta: { pageTitle: '咨询反馈' } },
+  { path: '/', component: Home, meta: { pageTitle: '首页' } },
+  { path: '/home', component: Home, meta: { pageTitle: '首页' } },
+  { path: '/func', component: Func, meta: { pageTitle: '功能' } },
+  { path: '/message', component: Message, meta: { pageTitle: '消息' } },
+  { path: '/my', component: My, meta: { pageTitle: '我的' } },
+  { path: '/about', component: About, meta: { pageTitle: '关于' } },
+  { path: '/func/selectIssueType', component: SelectIssueType, meta: { pageTitle: '咨询反馈' } },
 ]
 
 const router = createRouter({
